3601d36e7f46a9258f986b97f1141b81107f15eb
kyle.y <kyle.y@kakaobank.com>
2018-03-02 09:00:26 +0900

    0.0.5

--- src/test/kotlin/com/kyle/webservice/domain/WebControllerTest.kt
+++ src/test/kotlin/com/kyle/webservice/domain/WebControllerTest.kt
@@ -8,24 +8,22 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.test.context.junit4.SpringRunner
 
-//@RunWith(SpringRunner::class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//class WebControllerTest{
-//
-//    @Autowired
-//    private lateinit var restTemplate: TestRestTemplate
-//
-////    @Test
-////    fun 메인페이지_로딩(){
-//////        //when
-//////        val body = this.restTemplate.getForObject("/", String::class.java)
-//////
-//////        //then
-//////        assertThat(body).contains("스프링부트로 시작하는 웹 서비스")
-////    }
-//
-//
-//
-//}
-//
-// dd
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WebControllerTest{
+
+    @Autowired
+    private lateinit var restTemplate: TestRestTemplate
+
+    @Test
+    fun 메인페이지_로딩(){
+        //when
+        val body = this.restTemplate.getForObject("/", String::class.java)
+
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스")
+    }
+
+
+
+}
